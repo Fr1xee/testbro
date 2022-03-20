8c61da07b54e643164dd8e5e17fff76bfd105ce7
Fr1xee <73335583+Fr1xee@users.noreply.github.com>
2022-03-21 03:10:46 +0700

    Update blockchainActions.js

--- src/redux/blockchain/blockchainActions.js
+++ src/redux/blockchain/blockchainActions.js
@@ -4,12 +4,6 @@ import Web3 from "web3";
 // log
 import { fetchData } from "../data/dataActions";
 
-import Caver from 'caver-js';
-
-const caver = new Caver(window.klaytn);
-
-export default caver;
-
 const connectRequest = () => {
   return {
     type: "CONNECTION_REQUEST",
